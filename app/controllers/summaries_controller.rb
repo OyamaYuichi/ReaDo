876287c19312bad6ef6f4e789d1e7b29a78229cb
yuichi oyama <yuichi19991208@gmail.com>
2020-12-01 23:12:44 +0900

    new画面での書籍検索の修正

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,14 +1,14 @@
 class SummariesController < ApplicationController
   before_action :set_summary, only: [:show, :edit, :update, :destroy]
+  before_action :search, only: [:index, :new]
 
-  def index
-    @summaries = Summary.all
-
+  def search
     if params[:title].nil?
 
       searches = RakutenWebService::Books::Book.search(title: "Ruby")
     else
       searches = RakutenWebService::Books::Book.search(title: params[:title])
+      # redirect_to request.referer
     end
       searches_hits = searches.response
       # binding.pry
@@ -21,26 +21,50 @@ class SummariesController < ApplicationController
       @searches = Kaminari.paginate_array(@searches).page(params[:page]).per(10)
 
       @hit_count = searches.response["count"]
+
+      # redirect_to request.referer
+  end
+
+  def index
+    @summaries = Summary.all
+
+    # if params[:title].nil?
+
+    #   searches = RakutenWebService::Books::Book.search(title: "Ruby")
+    # else
+    #   searches = RakutenWebService::Books::Book.search(title: params[:title])
+    # end
+    #   searches_hits = searches.response
+    #   # binding.pry
+    #   @searches = []
+
+    #     searches_hits.each do |item|
+    #       book = item
+    #       @searches << book
+    #     end
+    #   @searches = Kaminari.paginate_array(@searches).page(params[:page]).per(10)
+
+    #   @hit_count = searches.response["count"]
   end
 
   def new
     @summary = Summary.new
 
-    if params[:title].nil?
+    # if params[:title].nil?
 
-      searches = RakutenWebService::Books::Book.search(title: "Ruby")
-    else
-      searches = RakutenWebService::Books::Book.search(title: params[:title])
-    end
-      searches_hits = searches.response
-      # binding.pry
-      @searches = []
+    #   searches = RakutenWebService::Books::Book.search(title: "Ruby")
+    # else
+    #   searches = RakutenWebService::Books::Book.search(title: params[:title])
+    # end
+    #   searches_hits = searches.response
+    #   # binding.pry
+    #   @searches = []
 
-        searches_hits.each do |item|
-          book = item
-          @searches << book
-        end
-      @searches = Kaminari.paginate_array(@searches).page(params[:page]).per(3)
+    #     searches_hits.each do |item|
+    #       book = item
+    #       @searches << book
+    #     end
+    #   @searches = Kaminari.paginate_array(@searches).page(params[:page]).per(3)
   end
 
   def create
