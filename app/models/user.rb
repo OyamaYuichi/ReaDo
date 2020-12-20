f89714332f0f7d27278615016ba376ea080dce3a
yuichi oyama <yuichi19991208@gmail.com>
2020-12-20 10:46:13 +0900

    userのModel Specを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,10 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  validates :name,  presence: true, length: { maximum: 15 }
+  validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
-  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password, presence: true, length: { minimum: 6 }
 
   class << self
     def current_user=(user)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'ユーザーモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'ユーザーネームが空の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: '', email: 'user@invalid.com', password: "password", uid: 1 )
+        expect(user).not_to be_valid
+      end
+    end
+    context 'ユーザーネームが30文字を越えていた場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: "a" * 31, email: 'user@invalid.com', password: "password", uid: 1 )
+        expect(user).not_to be_valid
+      end
+    end
+    context 'メールアドレスが空の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: 'user', email: '', password: "password", uid: 1 )
+        expect(user).not_to be_valid
+      end
+    end
+    context 'メールアドレスがフォーマット通りでない場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: 'user', email: 'user.email', password: "password", uid: 1 )
+        expect(user).not_to be_valid
+      end
+    end
+    context 'メールアドレスが255文字を越えていた場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: 'user', email: "a" * 255 + "@invalid.com", password: "password", uid: 1 )
+        expect(user).not_to be_valid
+      end
+    end
+    context 'パスワードが空の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: 'user', email: 'user@invalid.com', password: "", uid: 1 )
+        expect(user).not_to be_valid
+      end
+    end
+    context 'パスワードが6文字未満の場合' do
+      it 'バリデーションにひっかかる' do
+        user = User.new(name: 'user', email: 'user@invalid.com', password: "user", uid: 1 )
+        expect(user).not_to be_valid
+      end
+    end
+    context '内容が全て正しく記載されている場合' do
+      it 'バリデーションが通る' do
+        user = User.new(name: 'user', email: 'user@invalid.com', password: "password", uid: 1 )
+        expect(user).to be_valid
+      end
+    end
+  end
 end
