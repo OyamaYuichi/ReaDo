a8f5e4c11265d9d235ed363d30df6a3d70ddc716
yuichi oyama <yuichi19991208@gmail.com>
2020-12-24 00:29:10 +0900

    ゲストログイン機能を実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :check_guest, only: %i[update destroy]
+
+  def check_guest
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの変更・削除できません。'
+    end
+  end
+
   def build_resource(hash={})
     hash[:uid] = User.create_unique_string
     super
